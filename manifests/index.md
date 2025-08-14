eec47dee7a4e31f88474ed8d75f75546ffd3046e
Philip De Lorenzo <philip.delorenzo@gmail.com>
2025-08-14 15:56:23 -0700

    chore: change the app name

---
+++ manifests/index.md
@@ -0,0 +1,3 @@
+# PyOps Documentation Site (Deployment Documentation)
+
+For the local Kubernetes cluster (Kind) - see [pfo CLI](https://github.com/PyFlowOps/pfo-cli/blob/main/README.md).
---
+++ manifests/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# This script installs ADRift and its dependencies in a Kubernetes cluster.
+
+set -eou pipefail
+
+# This script assumes that kubectl and kustomize are installed and configured.
+# Namespace installation
+kubectl apply -f namespace.yaml
+
+# Check if the namespace was created successfully
+python -m pip install -r scripts/requirements.txt
+python scripts/patch-render.py # This gets the environment variables set up correctly for the ADRift application
+
+python scripts/secret-render.py --aws # This renders the ADRift secret file with the correct values | this project uses AWS credentials
+python scripts/docker-login.py --aws # This logs into the AWS ECR registry
+sleep 1
+python scripts/docker-download.py --aws # This downloads the Docker image from ECR
+sleep 1
+python scripts/github-ssh.py # This sets up the SSH keys for GitHub access
+
+if [[ $? -ne 0 ]]; then
+    echo "[ERROR] - Failed to render ADRift application patches. Please check the script output."
+    exit 1
+fi
+
+# Let's get the prerequisites ready
+echo "[INFO] - Installing prerequisites..."
+kustomize build prereqs | kubectl apply -f - > /dev/null 2>&1 || true
+sleep 3
+
+# We need to install the PostgreSQL storage first
+kubectl apply -f prereqs/postgres-pv.yaml > /dev/null 2>&1 || true
+sleep 3
+
+kubectl apply -f prereqs/postgres-pvc.yaml > /dev/null 2>&1 || true
+sleep 3
+
+# Install PostgreSQL
+echo "[INFO] - Installing PostgreSQL..."
+kustomize build postgres | kubectl apply -f - > /dev/null 2>&1 || true
+sleep 3
+
+# Install ADRift
+echo "[INFO] - Installing ADRift..."
+kustomize build overlays | kubectl apply -f - > /dev/null 2>&1 || true
+sleep 3
+
+echo "[COMPLETE] - ADRrift and its dependencies have been successfully installed!"
